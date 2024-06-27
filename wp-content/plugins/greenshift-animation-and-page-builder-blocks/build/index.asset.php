2926b97acfdce9c65ead93091520e3f946271f3d
DI-GIUSTO-Alex1987 <digiusto.alex@tutanota.com>
2024-06-27 09:31:00 +0200

    Seasons V1.2 Mise en place de la strucure de la page home

---
+++ wp-content/plugins/greenshift-animation-and-page-builder-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f576145ff38a7dc68551');
